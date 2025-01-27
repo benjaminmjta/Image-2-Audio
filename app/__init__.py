5bd95be47eff5952c595705f94e301e18a421563
benjaminmjta <112646910+benjaminmjta@users.noreply.github.com>
2025-01-27 14:20:18 +0100

    style
    added fontawesome some changes to ui

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 import os
+from flask_font_awesome import FontAwesome
+
+font_awesome = FontAwesome()
+
 
 def create_app():
     app = Flask(__name__)
     app.config['IMAGE_FOLDER'] = os.path.join('.', 'app', 'static', 'images')
     app.config['AUDIO_FOLDER'] = os.path.join('.', 'app', 'static', 'audios')
+
+    font_awesome.init_app(app)
 
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
